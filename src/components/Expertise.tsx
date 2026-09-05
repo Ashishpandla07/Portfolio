d4c392175a37be526c4989f1eb57ea39da94f49c
Ashish pandla <Ashishpandla07@gmail.com>
2026-09-05 23:30:23 +0530

    photo add

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -6,7 +6,7 @@ import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 
 const labelsFirst = [
-    "FP&A",
+    "Financial Planning & Analysis",
     "Budgeting & Forecasting",
     "Financial Modelling",
     "Variance Analysis",
