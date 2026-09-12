8b4d53a79554a02923342f37a1689cde3133e7d9
Ashish pandla <Ashishpandla07@gmail.com>
2026-09-12 07:15:12 +0530

    Update footer to keep only copyright line

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import EmailIcon from '@mui/icons-material/Email';
-import '../assets/styles/Footer.scss'
+import '../assets/styles/Footer.scss';
 
 function Footer() {
   return (
     <footer>
-      <div>
-        <a href="https://linkedin.com/in/ipandla" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
-        <a href="mailto:ashishpandla07@gmail.com" target="_blank" rel="noreferrer"><EmailIcon/></a>
-      </div>
-      <p>© 2026 Ashish Pandla · Chartered Accountant · Gurugram, Haryana</p>
+      <p>© 2026 Ashish Pandla, CA. Gurugram, Haryana. All rights reserved.</p>
     </footer>
   );
 }
